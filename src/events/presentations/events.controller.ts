33518ee99f848f7606df842024316e497f766726
Suyoung Kim <sykim@weflo.ai>
2024-07-10 13:53:44 +0900

    ✨ SWAGGER 추가

--- src/events/presentations/events.controller.ts
+++ src/events/presentations/events.controller.ts
@@ -1,89 +1,87 @@
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
-import { GetEventDatesDto } from './dto/get.event.dates';
 import dayjs from 'dayjs';
-import { GetEventDateSeatsDto } from './dto/get.event.date.seats';
-import { PostSeatReservationDto } from './dto/post.seat.reservation';
-import { SelectByRservationIdDto } from 'src/users/presentations/dto/select.by.reservation.id.dto';
+import { GetEventDatesDto, GetEventDateSeatsDto, PostSeatReservationDto, PutReservationDto } from './dto/events.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { EventDatesResponseDto, EventSeatsResponseDto, ReservationResponseDto } from './presenter/events.response.dto';
 
 @Controller('events')
 export class EventsController {
+  @ApiResponse({ type: EventDatesResponseDto, isArray: true })
+  @ApiOperation({ summary: '공연 날짜 조회' })
   @Get(':eventId/dates')
   async getEventDates(@Param() param: GetEventDatesDto) {
     const { eventId } = param;
-    return {
-      success: true,
-      data: [
-        {
-          id: 1,
-          eventId,
-          eventDate: dayjs(Date.now()).toDate(),
-          seatCount: 50,
-          bookStartDate: dayjs(Date.now()).toDate(),
-          bookEndDate: dayjs(Date.now()).toDate(),
-          createdAt: dayjs(Date.now()).toDate(),
-          updatedAt: dayjs(Date.now()).toDate(),
-          deletedAt: null,
-        },
-      ],
-    };
+    const result = [
+      {
+        id: 1,
+        eventId,
+        eventDate: dayjs(Date.now()).toDate(),
+        seatCount: 50,
+        bookStartDate: dayjs(Date.now()).toDate(),
+        bookEndDate: dayjs(Date.now()).toDate(),
+        createdAt: dayjs(Date.now()).toDate(),
+        updatedAt: dayjs(Date.now()).toDate(),
+        deletedAt: null,
+      },
+    ];
+    return Promise.all(result.map(async (entity) => await EventDatesResponseDto.fromDomain(entity)));
   }
 
+  @ApiResponse({ type: EventSeatsResponseDto, isArray: true })
+  @ApiOperation({ summary: '공연 날짜 하나의 좌석 조회' })
   @Get('properties/:propertyId/seats')
   async getEventDateSeats(@Param() param: GetEventDateSeatsDto) {
     const { propertyId } = param;
-    return {
-      success: true,
-      data: [
-        {
+    const result = [
+      {
+        id: 1,
+        seatNumber: 1,
+        status: 'AVAILABLE',
+        propertyId,
+        seatProperty: {
           id: 1,
-          seatNumber: 1,
-          status: 'AVAILABLE',
-          propertyId,
-          seatProperty: {
-            id: 1,
-            name: '스탠딩',
-            price: 100000,
-          },
+          name: '스탠딩',
+          price: 100000,
         },
-      ],
-    };
+      },
+    ];
+    return Promise.all(result.map(async (entity) => await EventSeatsResponseDto.fromDomain(entity)));
   }
 
+  @ApiBody({ type: PostSeatReservationDto })
+  @ApiResponse({ type: ReservationResponseDto })
+  @ApiOperation({ summary: '좌석 예약' })
   @Post('seats/reservation')
   async postSeatReservation(@Body() body: PostSeatReservationDto) {
     const { seatId, userId } = body;
-    return {
-      success: true,
-      data: {
-        id: 1,
-        seatId,
-        userId,
-        status: 'RESERVED',
-        eventId: 1,
-        eventName: '공연 이름',
-        eventPropertyId: 1,
-        eventDate: dayjs(Date.now()).toDate(),
-        price: 50000,
-      },
-    };
+    return await ReservationResponseDto.fromDomain({
+      id: 1,
+      seatId,
+      userId,
+      status: 'RESERVED',
+      eventId: 1,
+      eventName: '공연 이름',
+      eventPropertyId: 1,
+      eventDate: dayjs(Date.now()).toDate(),
+      price: 50000,
+    });
   }
 
+  @ApiResponse({ type: ReservationResponseDto })
+  @ApiOperation({ summary: '예약한 좌석 결제' })
   @Put('reservations/:reservationId/pay')
-  async paySeatReservation(@Param() param: SelectByRservationIdDto) {
+  async putSeatReservation(@Param() param: PutReservationDto) {
     const { reservationId } = param;
-    return {
-      success: true,
-      data: {
-        id: reservationId,
-        seatId: 1,
-        userId: 'ffd7a6d2-b742-4b7c-b7e4-a5e435435288',
-        status: 'PAID',
-        eventId: 1,
-        eventName: '공연 이름',
-        eventPropertyId: 1,
-        eventDate: dayjs(Date.now()).toDate(),
-        price: 50000,
-      },
-    };
+    return ReservationResponseDto.fromDomain({
+      id: reservationId,
+      seatId: 1,
+      userId: 'ffd7a6d2-b742-4b7c-b7e4-a5e435435288',
+      status: 'PAID',
+      eventId: 1,
+      eventName: '공연 이름',
+      eventPropertyId: 1,
+      eventDate: dayjs(Date.now()).toDate(),
+      price: 50000,
+    });
   }
 }
--- src/queues/presentations/queues.controller.ts
+++ src/queues/presentations/queues.controller.ts
@@ -1,15 +1,37 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateQueueDto } from './dto/create.queue.dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateQueueDto, GetQueueDto } from './dto/queue.dto';
 import dayjs from 'dayjs';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { QueueResponseDto } from './presenter/queue.response.dto';
 
 @Controller('queues')
 export class QueuesController {
+  @ApiBody({ type: CreateQueueDto })
+  @ApiResponse({ type: QueueResponseDto, status: 201 })
+  @ApiOperation({ summary: '대기열 생성' })
   @Post()
   async createQueue(@Body() body: CreateQueueDto) {
-    const { userId } = body;
-    return {
-      success: true,
-      data: { id: 1, userId, eventId: 1, status: 'STAND_BY', createdAt: dayjs(Date.now()).toDate() },
-    };
+    const { userId, eventId } = body;
+    return await QueueResponseDto.fromDomain({
+      id: 1,
+      userId,
+      eventId,
+      status: 'STANDBY',
+      createdAt: dayjs(Date.now()).toDate(),
+    });
+  }
+
+  @ApiResponse({ type: QueueResponseDto, status: 200 })
+  @ApiOperation({ summary: '대기열 조회' })
+  @Get('users/:userId/events/:eventId')
+  async getQueue(@Param() param: GetQueueDto) {
+    const { userId, eventId } = param;
+    return await QueueResponseDto.fromDomain({
+      id: 1,
+      userId,
+      eventId,
+      status: 'STANDBY',
+      createdAt: dayjs(Date.now()).toDate(),
+    });
   }
 }
