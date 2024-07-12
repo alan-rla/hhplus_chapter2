c9e1c3e45ba09dfddeece689834e78f50df3a020
Suyoung Kim <sykim@weflo.ai>
2024-07-12 14:46:21 +0900

    🔥파일 이름 변경

--- src/users/domain/repositories/user.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Queue } from '../models/queue.model';
-
-export abstract class QueuesRepository {
-  abstract post(userId: string, eventId: number): Promise<Queue>;
-
-  abstract getByUserIdAndEventId(userId: string, eventId: number): Promise<Queue>;
-}
