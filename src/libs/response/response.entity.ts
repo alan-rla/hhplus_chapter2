6181650dfd5359f62939a4f6cd646fe8138daf8f
Suyoung Kim <sykim@weflo.ai>
2024-07-17 16:20:23 +0900

    ♻️ Mapper 통합
    ♻️ Mapper 통합
    
    ♻️ Mapper 통합

--- src/libs/response/response.entity.ts
+++
@@ -1,9 +0,0 @@
-import { plainToInstance } from 'class-transformer';
-import { validate } from 'class-validator';
-
-export async function createResponse<T>(responseClass: new () => T, data: object): Promise<T> {
-  const entity: any = plainToInstance(responseClass, data, { excludeExtraneousValues: true, exposeUnsetFields: false });
-  const [error] = await validate(entity);
-  if (error) throw error;
-  return entity;
-}
