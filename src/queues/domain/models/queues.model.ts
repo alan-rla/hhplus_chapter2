bd24eac947bc3a6ba674776b79710d4aba974828
Suyoung Kim <sykim@weflo.ai>
2024-07-18 21:01:13 +0900

    🔥 Removed @Expose() for models

--- src/queues/domain/models/queues.model.ts
+++ src/queues/domain/models/queues.model.ts
@@ -1,63 +1,39 @@
-import { Expose } from 'class-transformer';
 import { QueueStatusEnum } from '../../../libs/types';
-import { IsDate, IsEnum, IsNumber, IsString, IsUUID } from 'class-validator';
+import { IsDate, IsEnum, IsNumber, IsOptional, IsString, IsUUID, ValidateIf } from 'class-validator';
 
 export class QueueRequestProps {
-  @Expose()
   @IsUUID()
+  @IsOptional()
   userId?: string;
-  @Expose()
+
   @IsNumber()
   eventId: number;
-  @Expose()
+
   @IsEnum(QueueStatusEnum)
+  @IsOptional()
   status?: QueueStatusEnum;
 }
 
-export class QueueResponseProps {
-  @Expose()
-  @IsNumber()
-  id: number;
-  @Expose()
-  @IsUUID()
-  userId: string;
-  @Expose()
-  @IsNumber()
-  eventId: number;
-  @Expose()
-  @IsEnum(QueueStatusEnum)
-  status: QueueStatusEnum;
-  @Expose()
-  @IsDate()
-  createdAt: Date;
-  @Expose()
-  @IsDate()
-  updatedAt: Date;
-  @Expose()
-  @IsDate()
-  deletedAt: Date;
-}
-
 export class Queue {
-  @Expose()
   @IsNumber()
   id: number;
-  @Expose()
+
   @IsString()
   userId: string;
-  @Expose()
+
   @IsNumber()
   eventId: number;
-  @Expose()
+
   @IsEnum(QueueStatusEnum)
   status: QueueStatusEnum;
-  @Expose()
+
   @IsDate()
   createdAt: Date;
-  @Expose()
+
   @IsDate()
   updatedAt: Date;
-  @Expose()
+
   @IsDate()
+  @ValidateIf((o) => o.deletedAt !== null)
   deletedAt: Date;
 }
--- src/users/domain/models/users.model.ts
+++ src/users/domain/models/users.model.ts
@@ -1,57 +1,52 @@
-import { Expose } from 'class-transformer';
 import { BalanceTypeEnum } from '../../../libs/types';
 import { IsDate, IsEnum, IsNumber, IsUUID } from 'class-validator';
 
 export class UserBalanceProps {
-  @Expose()
   @IsNumber()
   id?: number;
-  @Expose()
+
   @IsUUID()
   userId?: string;
-  @Expose()
+
   @IsNumber()
   balance?: number;
 }
 
 export class BalanceHistoryProps {
-  @Expose()
   @IsUUID()
   userId: string;
-  @Expose()
+
   @IsNumber()
   amount: number;
-  @Expose()
+
   @IsEnum(BalanceTypeEnum)
-  type: BalanceTypeEnum;
+  type?: BalanceTypeEnum;
 }
 
 export class UserBalance {
-  @Expose()
   @IsNumber()
   id: number;
-  @Expose()
+
   @IsUUID()
   userId: string;
-  @Expose()
+
   @IsNumber()
   balance: number;
 }
 
 export class BalanceHistory {
-  @Expose()
   @IsNumber()
   id: number;
-  @Expose()
+
   @IsUUID()
   userId: string;
-  @Expose()
+
   @IsEnum(BalanceTypeEnum)
   type: BalanceTypeEnum;
-  @Expose()
+
   @IsNumber()
   amount: number;
-  @Expose()
+
   @IsDate()
   createdAt: Date;
 }
