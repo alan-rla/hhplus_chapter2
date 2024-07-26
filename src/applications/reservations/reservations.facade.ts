0fef97bda5663d62b195a9d00f577e779a2f8017
Suyoung Kim <sykim@weflo.ai>
2024-07-26 13:50:55 +0900

    ✨ redis lock 적용 facade

--- src/applications/reservations/reservations.facade.ts
+++ src/applications/reservations/reservations.facade.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { EventsService } from '@src/domains/events/events.service';
+import { RedisLocksService } from '@src/domains/redis.locks/redis.locks.service';
 import { Reservation } from '@src/domains/reservations/reservations.model';
 import { ReservationsService } from '@src/domains/reservations/reservations.service';
+import { Seat } from '@src/domains/seats/seats.model';
 import { SeatsService } from '@src/domains/seats/seats.service';
+import { Transactional } from 'typeorm-transactional';
 
 @Injectable()
 export class ReservationsFacade {
@@ -10,10 +13,19 @@ export class ReservationsFacade {
     private readonly seatsService: SeatsService,
     private readonly eventsService: EventsService,
     private readonly reservationsService: ReservationsService,
+    private readonly redisLocksService: RedisLocksService,
   ) {}
 
   async postReservation(seatId: number, userId: string): Promise<Reservation> {
     const seat = await this.seatsService.getSeatById(seatId);
+    const key = `EVENT_PROPERTY_LOCK:ID_${seat.eventPropertyId}`;
+    const reservation = await this.postReservationTransaction(seat, userId);
+    await this.redisLocksService.removeLock(key, userId);
+    return reservation;
+  }
+
+  @Transactional()
+  async postReservationTransaction(seat: Seat, userId: string): Promise<Reservation> {
     const occupiedSeat = await this.seatsService.putSeatStatusToOccupied(seat);
     const eventProperty = await this.eventsService.getEventPropertyById(occupiedSeat.eventPropertyId);
     return await this.reservationsService.postReservation(occupiedSeat, eventProperty, userId);
--- src/applications/users/users.facade.ts
+++ src/applications/users/users.facade.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { RedisLocksService } from '@src/domains/redis.locks/redis.locks.service';
 import { BalanceHistory, UserBalance } from '@src/domains/users/users.model';
 import { UsersService } from '@src/domains/users/users.service';
-import { Transactional } from 'typeorm-transactional';
 
 @Injectable()
 export class UsersFacade {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly redisLocksService: RedisLocksService,
+  ) {}
 
   async getUserBalanceByUserId(userId: string): Promise<UserBalance> {
     return await this.usersService.getUserBalanceByUserId(userId);
   }
 
-  @Transactional()
   async charge(userId: string, amount: number): Promise<BalanceHistory> {
-    const userBalance = await this.usersService.getUserBalanceByUserId(userId);
-    return await this.usersService.charge(userId, userBalance.balance, amount);
+    const userBalance = await this.usersService.getUserBalanceByUserId(userId, true);
+    const key = `POINT_LOCK:ID_${userId}`;
+    const acquireLock = await this.redisLocksService.acquireLock(key, userId);
+    if (acquireLock) {
+      const chargeHistory = await this.usersService.charge(userId, userBalance.balance, amount);
+      await this.redisLocksService.removeLock(key, userId);
+      return chargeHistory;
+    } else {
+      await this.redisLocksService.addUserToSubscription(key, userId);
+      return this.redisLocksService.listenMessage(key, userId).then(async () => {
+        const chargeHistory = await this.usersService.charge(userId, userBalance.balance, amount);
+        await this.redisLocksService.removeLock(key, userId);
+        return chargeHistory;
+      });
+    }
   }
 }
